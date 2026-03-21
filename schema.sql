eb0e314550eec34922e659cfcc63ba5e00831791
gtpic <1301701321@qq.com>
2026-03-22 00:20:39 +0800

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,22 +1,6 @@
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    tg_topic_id INTEGER,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS messages (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER,
-    sender TEXT, 
-    content TEXT,
-    is_read INTEGER DEFAULT 0, 
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS config (
-    key TEXT PRIMARY KEY,
-    value TEXT
-);
+CREATE TABLE config (     key TEXT PRIMARY KEY,     value TEXT )
+CREATE TABLE messages (     id INTEGER PRIMARY KEY AUTOINCREMENT,     user_id INTEGER,     sender TEXT,      content TEXT,     is_read INTEGER DEFAULT 0,      created_at DATETIME DEFAULT CURRENT_TIMESTAMP )
+CREATE TABLE users (     id INTEGER PRIMARY KEY AUTOINCREMENT,     tg_topic_id INTEGER,     created_at DATETIME DEFAULT CURRENT_TIMESTAMP )
 
 INSERT OR IGNORE INTO config (key, value) VALUES ('admin_username', 'admin');
 INSERT OR IGNORE INTO config (key, value) VALUES ('admin_password', '123456');
